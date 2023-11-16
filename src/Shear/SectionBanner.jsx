300e42d825b854d53228881aeec6edbafed22658
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-16 11:42:18 +0600

    added footer section

--- src/Shear/SectionBanner.jsx
+++ src/Shear/SectionBanner.jsx
@@ -4,7 +4,7 @@ const SectionBanner = ({ title, des, img }) => {
       <img src={img} alt="" className="h-full w-full" />
       <div className="absolute p-20 top-1/4 left-52 w-8/12 text-center bg-white ">
         <h2 className="text-5xl font-normal">{title}</h2>
-        <p className="text-base font-normal">{des}</p>
+        <p className="text-base font-normal mt-5">{des}</p>
       </div>
     </div>
   );
