c90121792e02ad14d9c152f316e80b068e12886f
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-18 15:03:54 +0600

    added sign up page

--- src/Pages/Signup/SignUp.jsx
+++ src/Pages/Signup/SignUp.jsx
@@ -1,10 +1,81 @@
+import { NavLink } from "react-router-dom";
 
 const SignUp = () => {
-    return (
-        <div>
-            Sign up
+  const handleSignupFrom = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+  };
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content w-full">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <h1 className="text-4xl text-center mt-6 font-bold">Sign Up</h1>
+            <form onSubmit={handleSignupFrom} className="card-body">
+              {/* name */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              {/* email */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              {/* password */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control mt-6">
+                <button
+                  type="submit"
+                  className="btn bg-[#D1A054] hover:bg-[#e6bd7f] text-white"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+            <p className="text-center mb-6 text-xl font-medium">
+              Already registered?{" "}
+              <NavLink to={"/login"} className={"text-[#D1A054]"}>
+                Go to log in
+              </NavLink>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
