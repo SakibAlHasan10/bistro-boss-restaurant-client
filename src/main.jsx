d8351ce3cb8a21917cdef8e0b85436aa366a7da7
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-18 08:54:24 +0600

    create baseurl hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routers from './Routers/Routers/Routers'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routers from "./Routers/Routers/Routers";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routers}/>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routers} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
