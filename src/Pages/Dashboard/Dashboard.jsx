78233dba49eb57e29f3afb71fa17f0a5173d5b0d
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-19 11:55:10 +0600

    added user dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,33 @@
+import { NavLink } from "react-router-dom";
+import Area from "../../Shear/area";
 
 const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
+//   const adminMenu = ["admin home"];
+  const userMenu = ["user home", "reservation", "payment history", "my cart", "add review", "my booking"];
+  return (
+    <Area>
+      <div className="flex text-black max-h-screen h-[100vh]">
+        <div className="w-72 bg-[#D1A054] p-6">
+          rouret
+          {userMenu?.map((nav) => (
+            <div key={nav} className=" uppercase list-none text-base font-medium">
+              <li className="my-3">
+                <NavLink
+                  to={`Dashboard/${nav}`}
+                  className={({ isActive, isPending }) =>
+                    isActive ? "active" : isPending ? "pending" : ""
+                  }
+                >
+                    {nav}
+                </NavLink>
+              </li>
+            </div>
+          ))}
         </div>
-    );
+        <div className="w-full bg-red-400 p-6">content</div>
+      </div>
+    </Area>
+  );
 };
 
 export default Dashboard;
--- src/Routers/Routers/Routers.jsx
+++ src/Routers/Routers/Routers.jsx
@@ -7,42 +7,49 @@ import Contact from "../../Pages/Contact/Contact";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/Signup/SignUp";
+import Admin from "../../Layout/AdminDashboard/Admin";
 
 const routers = createBrowserRouter([
-        {
-            path:'/',
-            element:<Roots/>,
-            children:[
-                {
-                    path:'/',
-                    element:<Home/>
-                },
-                {
-                    path:"/Menu",
-                    element:<OurMenu></OurMenu>
-                },
-                {
-                    path:"/Shop",
-                    element:<OurShop/>
-                },
-                {
-                    path:"/Contact",
-                    element:<Contact/>
-                },
-                {
-                    path:"Dashboard",
-                    element:<Dashboard/>
-                }
-            ]
-        },
-        {
-            path:"/login",
-            element:<Login/>
-        },
-        {
-            path:"signUp",
-            element:<SignUp/>
-        }
-    ])
+  {
+    path: "/",
+    element: <Roots />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Menu",
+        element: <OurMenu></OurMenu>,
+      },
+      {
+        path: "/Shop",
+        element: <OurShop />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "Dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/Dashboard/admin",
+        element: <Admin />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "signUp",
+    element: <SignUp />,
+  },
+]);
 
 export default routers;
