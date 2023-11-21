dccc0326581dc1c9ae8aaf8ff10fd306a430e897
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-21 09:29:01 +0600

    added usewr dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -45,7 +45,7 @@ const nav = (
   </>
 );
 const Dashboard = () => {
-  const isAdmin = true;
+  const isAdmin = false;
   const adminMenu = [
     "admin home",
     "add items",
@@ -63,64 +63,89 @@ const Dashboard = () => {
   ];
   return (
     <Area>
-      <div className="flex text-black max-h-screen h-[100vh]">
-        <div className="w-72 bg-[#D1A054] p-6">
-          rouret
-          <div>
-            {isAdmin
-              ? adminMenu?.map((nav) => (
-                  <div
-                    key={nav}
-                    className=" uppercase list-none text-base font-medium"
-                  >
-                    <li className="hover:text-yellow-400 my-3 hover:border-yellow-400">
-                      <NavLink
-                        to={`${nav}`}
-                        className={({ isActive, isPending }) =>
-                          isActive
-                            ? "active text-yellow-400"
-                            : isPending
-                            ? "pending"
-                            : ""
-                        }
+      {/* <div className="flex text-black max-h-screen h-[100vh]"></div> */}
+      {/* drawer */}
+      <div>
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col items-center justify-center">
+            {/* Page content here */}
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-primary drawer-button lg:hidden"
+            >
+              Open drawer
+            </label>
+            <div className="w-full bg-white p-6">
+              <Outlet />
+            </div>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+
+            {/* <ul className="menu p-4 w-80 min-h-full bg-[#D1A054] text-base-content "> */}
+            {/* Sidebar content here */}
+            <div className="menu p-4 text-black w-80 min-h-full bg-[#D1A054] ">
+              <div>
+                {isAdmin
+                  ? adminMenu?.map((nav) => (
+                      <div
+                        key={nav}
+                        className=" uppercase list-none text-base font-medium"
                       >
-                        {nav}
-                      </NavLink>
-                    </li>
-                  </div>
-                ))
-              : userMenu?.map((nav) => (
-                  <div
-                    key={nav}
-                    className=" uppercase list-none text-base font-medium"
-                  >
-                    <li className="hover:text-yellow-400 my-3 hover:border-yellow-400">
-                      <NavLink
-                        to={`${nav}`}
-                        className={({ isActive, isPending }) =>
-                          isActive
-                            ? "active text-yellow-400"
-                            : isPending
-                            ? "pending"
-                            : ""
-                        }
+                        <li className="">
+                          <NavLink
+                            to={`${nav}`}
+                            className={({ isActive, isPending }) =>
+                              isActive
+                                ? "active text-yellow-400"
+                                : isPending
+                                ? "pending"
+                                : ""
+                            }
+                          >
+                            {nav}
+                          </NavLink>
+                        </li>
+                      </div>
+                    ))
+                  : userMenu?.map((nav) => (
+                      <div
+                        key={nav}
+                        className=" uppercase list-none text-base font-medium"
                       >
-                        {nav}
-                      </NavLink>
-                    </li>
-                  </div>
-                ))}
+                        <li className="">
+                          <NavLink
+                            to={`${nav}`}
+                            className={({ isActive, isPending }) =>
+                              isActive
+                                ? "active text-yellow-400"
+                                : isPending
+                                ? "pending"
+                                : "hover:text-yellow-400 my-1 hover:border-yellow-400"
+                            }
+                          >
+                            {nav}
+                          </NavLink>
+                        </li>
+                      </div>
+                    ))}
+              </div>
+              {/* divider */}
+              <div className=" text-white w-full">
+                <p className="border-b my-6 border-white w-full"></p>
+              </div>
+              <div className=" uppercase list-none text-base font-medium">
+                {nav}
+              </div>
+            </div>
+
+            {/* </ul> */}
           </div>
-          {/* divider */}
-          <div className=" text-white w-full">
-            <p className="border-b my-6 border-white w-full"></p>
-          </div>
-          <div className=" uppercase list-none text-base font-medium">
-            {nav}
-          </div>
-        </div>
-        <div className="w-full bg-white p-6">
-          <Outlet />
         </div>
       </div>
     </Area>
